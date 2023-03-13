a2d6aad95ea2d00e65981b54c3dfe899f63126c3
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-13 18:12:55 -0300

    Agregando logica ejercicio01

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -1,3 +1,24 @@
 function ejercicio01(el){
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <h2>Ejercicio 1</h2>
+    <button class="boton">Listar meses</button>
+    <div class="resultado"></div>
+    `;
+
+    const boton = articleEl.querySelector(".boton");
     
+    boton.addEventListener("click", () => {
+        const resultado = articleEl.querySelector(".resultado");
+        let texto = "";
+        let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+        texto += "<ul>"
+        for (let i = 0; i < meses.length; i++) {
+            texto += `<li>${meses[i]}</li>`
+        }
+        texto += "</ul>";
+        resultado.innerHTML = texto;
+    });
+    el.appendChild(articleEl);
 }
