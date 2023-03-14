9a2aad60efda31519a938545854e79899863508d
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-13 21:47:13 -0300

    Agregando logica ejercicio03

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -1,3 +1,41 @@
-function ejercicio03(){
-    
+function ejercicio03() {
+  const articleEl = document.createElement("article");
+  articleEl.innerHTML = `
+      <h2>Ejercicio 3</h2>
+      <form class="form">
+          <div>
+              <label for="base">Ingrese el valor de la base del rectangulo</label>
+              <input class="base" name="base" type="text">
+          </div>
+          <div>
+              <label for="altura">Ingrese el valor de la altura del rectangulo</label>
+              <input class="altura" name="altura" type="text">
+          </div>
+          <button>Calcular perímetro</button>
+      </form>
+      <p class="resultado"></p>
+    `;
+
+  const formEl = articleEl.querySelector(".form");
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const base = parseInt(articleEl.querySelector(".base").value);
+    const altura = parseInt(articleEl.querySelector(".altura").value);
+    const resultado = articleEl.querySelector(".resultado");
+    let texto = "";
+
+    if (
+      !isNaN(base) &&
+      !isNaN(altura) &&
+      base >= 1 &&
+      altura >= 1
+    ) {
+      resultado.textContent = `El perimetro del rectangulo ingresado es ${2*(base+altura)}`
+    } else {
+      resultado.textContent = "Debe ingresar numeros validos!";
+    }
+  });
+
+  return articleEl;
 }
